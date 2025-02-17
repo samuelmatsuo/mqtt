90a33c3b4330f57148eff94c32ecf2a361a1142c
Samuel Bampi Matsuo <samuel.matsuo@ixcsoft.com.br>
2025-02-17 17:50:51 -0300

    fix(connetion): update update hivemq

--- src/mqttClient.ts
+++ src/mqttClient.ts
@@ -1,17 +1,21 @@
 import mqtt from "mqtt";
 import dotenv from "dotenv";
+import fs from "fs";
 
 dotenv.config();
 
 const connectToBroker = () => {
   const brokerUrl = process.env.BROKER_URL || "ssl://broker.hivemq.com";
-
   const options = {
-    username: process.env.MQTT_USERNAME,
-    password: process.env.MQTT_PASSWORD,
+    protocol: "mqtts",
+    host: "broker.hivemq.com",
+    port: 8883,
+    ca: [fs.readFileSync("/path/to/ca.crt")],
+    cert: fs.readFileSync("/path/to/client.crt"),
+    key: fs.readFileSync("/path/to/client.key"),
   };
 
-  const client = mqtt.connect(brokerUrl, options);
+  const client = mqtt.connect(options);
 
   client.on("connect", () => {
     console.log("Connectado ao broker MQTT");
